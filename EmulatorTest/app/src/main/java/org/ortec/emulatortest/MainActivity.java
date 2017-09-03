2a2158ee59650b9d8d9e0aefc4fcf46ab87537cf
Andrej Treskow <treskov@ortec.org>
2017-09-03 10:53:21 +0200

    Settings - Menu in MainActivity

--- EmulatorTest/app/src/main/java/org/ortec/emulatortest/MainActivity.java
+++ EmulatorTest/app/src/main/java/org/ortec/emulatortest/MainActivity.java
@@ -3,6 +3,8 @@ package org.ortec.emulatortest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -35,5 +37,34 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, RestApiTestActivity.class);
         startActivity(i);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+/*
+    @Override
+    public boolean OnCreateOptionsMenu(Menu menu) {
+        //getMenuInflater(R.menu.menu_activity_main, menu);
+        return true;
+    }
+*/
 
 }
