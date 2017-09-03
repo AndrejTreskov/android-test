728e347c106573e31ef71fb959958f5260cb7b37
Andrej Treskow <treskov@ortec.org>
2017-09-03 08:18:00 +0200

    Circular ProgressBar - Spinner (anzeige beim Click auf den Button)

--- EmulatorTest/app/src/main/java/org/ortec/emulatortest/MainActivity.java
+++ EmulatorTest/app/src/main/java/org/ortec/emulatortest/MainActivity.java
@@ -2,12 +2,27 @@ package org.ortec.emulatortest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mButton;
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button)findViewById(R.id.button);
+        mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.GONE);
     }
+
+    public void onButtonClick(View v) {
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
 }
